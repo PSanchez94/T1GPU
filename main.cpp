a266947f6a0ff25a21612259ff2dda5a8e282125
PSanchez94 <pablo.sanchez.r@ug.uchile.cl>
2021-05-01 03:35:26 -0400

    Namespaced

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@
 
 #include "Poligono.hpp"
 
+using namespace std;
+
 Poligono<float>* main_poligono;
 
 int main() {
@@ -16,22 +18,22 @@ int main() {
     Punto<float>* mp = new Punto<float>();
     Punto<float>* rp = new Punto<float>();
 
-    std::cout << "Implementation of Poligono Class" << std::endl;
-    std::cout << "A Poligono must have at least 3 points." << std::endl;
-    std::cout << "Enter X and Y values for the first 3 Puntos." << std::endl;
-    std::cout << "Press enter when done." << std::endl;
+    cout << "Implementation of Poligono Class" << endl;
+    cout << "A Poligono must have at least 3 points." << endl;
+    cout << "Enter X and Y values for the first 3 Puntos." << endl;
+    cout << "Press enter when done." << endl;
 
-    std::cin >> *lp >> *mp >> *rp;
+    cin >> *lp >> *mp >> *rp;
 
     main_poligono = new Poligono<float>(lp, mp, rp);
-    std::cout << *main_poligono;
+    cout << *main_poligono;
 
-    std::cout << "Press enter to end the program or input X and Y values" << std::endl;
-    std::cout << "to append a Punto to the Poligono." << std::endl;
+    cout << "Press enter to end the program or input X and Y values" << endl;
+    cout << "to append a Punto to the Poligono." << endl;
 
     while (true) {
-        std::cin >> *main_poligono;
-        std::cout << *main_poligono;
+        cin >> *main_poligono;
+        cout << *main_poligono;
     }
 
     return 0;
